2122fa94fa437d5c788a9625bec21e184c48c2f8
Joschka Koester <jkoester@tzi.de>
2018-05-14 01:07:53 +0200

    [ lang ] complete script
    complete rewrite of script lang. will use output of `setxkbmap`

--- Arch-i3blocks/lang.sh
+++ Arch-i3blocks/lang.sh
@@ -1,3 +1,12 @@
 #!/bin/bash 
+# lang - displays keyboard layout
+# author: hringriin 
 
-echo -e "  $LANGUAGE"
+layout=`setxkbmap -query | grep -i layout | sed -e 's/  *//g' | cut -d ":" -f 2`
+variant=`setxkbmap -query | grep -i variant | sed -e 's/  *//g' | cut -d ":" -f 2`
+
+if [[ -z ${variant} ]] ; then
+    echo " ${layout}"
+else
+    echo " ${layout}_${variant}"
+fi
