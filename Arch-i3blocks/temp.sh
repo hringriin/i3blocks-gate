766f2ea8e8822bb3554ce9e3168c239269911479
Joschka Koester <jkoester@tzi.de>
2018-05-15 00:32:22 +0200

    [ temp ] improves / adds features to temp
    not very general, because it's custom fit to my machine. it will
    probably not work on any other. have to fine tune that.

--- Arch-i3blocks/temp.sh
+++ Arch-i3blocks/temp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash 
+# author: hringriin
 
-temp=$(sensors | grep "^CPU" | awk '{print $2}' | tr -d "+" )
+#temp=$(sensors | grep "^CPU" | awk '{print $2}' | tr -d "+" )
+#
+#echo -e " $temp"
 
-echo -e " $temp"
+core0=$(sensors | grep "Core 0" | sed -e 's/  */|/g' | cut -d "|" -f 3 | sed -e 's/+//g')
+core1=$(sensors | grep "Core 1" | sed -e 's/  */|/g' | cut -d "|" -f 3 | sed -e 's/+//g')
+temp1=$(sensors | grep "temp1" | sed -e 's/  */|/g' | cut -d "|" -f 2 | sed -e 's/+//g')
+fan1=$(sensors | grep "fan1" | sed -e 's/  *//g' | cut -d ":" -f 2 | sed -e 's/+//g' | sed -e 's/R/ R/g')
+
+echo " Core 0: ${core0} |  Core 1: ${core1} |  Temp 1: ${temp1} | Fan 1: ${fan1}"
