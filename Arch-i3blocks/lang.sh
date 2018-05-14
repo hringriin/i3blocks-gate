136225202e94e0f9dafd2b0f022a19105722af67
Joschka Koester <jkoester@tzi.de>
2018-05-15 00:29:40 +0200

    [ lang ] misused this script
    this script was once thought of returning the lang variable. however, i
    rewrote it to display my keyboard layout.

--- Arch-i3blocks/lang.sh
+++ Arch-i3blocks/lang.sh
@@ -6,7 +6,7 @@ layout=`setxkbmap -query | grep -i layout | sed -e 's/  *//g' | cut -d ":" -f 2`
 variant=`setxkbmap -query | grep -i variant | sed -e 's/  *//g' | cut -d ":" -f 2`
 
 if [[ -z ${variant} ]] ; then
-    echo " ${layout}"
+    echo "  ${layout}"
 else
-    echo " ${layout}_${variant}"
+    echo "  ${layout}_${variant}"
 fi
