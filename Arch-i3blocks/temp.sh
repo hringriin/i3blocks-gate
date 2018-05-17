58f563601faa990a5bb61d6b11d8a376ee8f14f6
Joschka Koester <jkoester@tzi.de>
2018-05-17 12:46:49 +0200

    [ temp.sh ] reducing output to make a shorter line

--- Arch-i3blocks/temp.sh
+++ Arch-i3blocks/temp.sh
@@ -10,4 +10,4 @@ core1=$(sensors | grep "Core 1" | sed -e 's/  */|/g' | cut -d "|" -f 3 | sed -e 
 temp1=$(sensors | grep "temp1" | sed -e 's/  */|/g' | cut -d "|" -f 2 | sed -e 's/+//g')
 fan1=$(sensors | grep "fan1" | sed -e 's/  *//g' | cut -d ":" -f 2 | sed -e 's/+//g' | sed -e 's/R/ R/g')
 
-echo " Core 0: ${core0} |  Core 1: ${core1} |  Temp 1: ${temp1} | Fan 1: ${fan1}"
+echo " C0: ${core0} |  C1: ${core1} |  T1: ${temp1}"
