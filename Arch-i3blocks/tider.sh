6be645bdf6a36d0e921f1bd5aafc32ef566da809
Joschka Koester <jkoester@tzi.de>
2018-06-25 05:28:09 +0200

    [ tider ] adding script
    tider is a time tracking software. this script evaluates the crappy
    output of the script to be useful. i like the idea of the software
    though.

---
+++ Arch-i3blocks/tider.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# tider
+
+raw=`cat ${HOME}/.config/tider/i3bar.txt`
+tstatus=`echo ${raw} | tr -d '"{}' | cut -d " " -f 2`
+tcolor=`echo ${raw} | tr -d '"{}' | cut -d "," -f 2 | cut -d " " -f 3`
+ttime=`echo ${raw} | tr -d '"{}' | cut -d "," -f 1 | sed -e 's/.*TIME //' | cut -d " " -f 1`
+tsubj=`echo ${raw} | tr -d '"{}' | cut -d "," -f 1 | sed -e 's/.*TIME //g' | sed -e 's/ /|/' | cut -d "|" -f 2`
+
+function test()
+{
+    echo -e "RAW DATA:  ${raw}"
+    echo -e "Status:    ${tstatus}"
+    echo -e "Color:     ${tcolor}"
+    echo -e "Time:      ${ttime}"
+    echo -e "Subject:   ${tsubj}"
+}
+
+if [[ $1 == "test" ]] ; then
+    test
+fi
+
+# button handling
+if [[ $1 == "1" ]] ; then
+    tider call report > /dev/null
+fi
+
+if [[ $1 == "2" ]] ; then
+    tider call target > /dev/null
+fi
+
+if [[ $1 == "3" ]] ; then
+    tider call menu > /dev/null
+fi
+
+echo "<span color=\"${tcolor}\">${ttime} ${tstatus} ${tsubj}</span>"
