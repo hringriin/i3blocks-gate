2bdde00b275baf51a67e984ccff0c486d5221b6f
Joschka Koester <jkoester@tzi.de>
2018-12-07 23:22:22 +0100

    [ volume ] redoing the volume script

--- Arch-i3blocks/vol.sh
+++ Arch-i3blocks/vol.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-Vol=$(amixer -c 0 get Master | grep "Mono:" | awk '{print $4}' | tr -d "[ %]")
-Mute=$(amixer -c 0 get Master | grep "Mono:" | awk '{print $6}' | tr -d "[-]")
+#Vol=$(amixer -c 0 get Master | grep "Mono:" | awk '{print $4}' | tr -d "[ %]")
+#Mute=$(amixer -c 0 get Master | grep "Mono:" | awk '{print $6}' | tr -d "[-]")
 
-if [ "$Mute" = "off" ];then
-    echo -e " Mute"
+if [[ $1 == "vol" ]] ; then
+    Vol=$(pulsemixer --get-volume | awk '{print $1}')
+    Mute=$(pulsemixer --get-mute)
+elif [[ $1 == "mic" ]] ; then
+    Vol=$(pulsemixer --id 1 --get-volume | awk '{print $1}')
+    Mute=$(pulsemixer --id 1 --get-mute)
+fi
+
+
+if [[ "$Mute" -eq 1 ]] ; then
+    echo -e "  Mute"
 else
     if [ "$Vol" -gt "80" ];then
         echo -e " $Vol %"
@@ -21,3 +30,5 @@ fi
 
 unset Vol
 unset Mute
+
+pkill -RTMIN+12 i3blocks
