c01bb0d2a6e5d3594668769f49b653481aaaab91
Joschka Koester <jkoester@tzi.de>
2018-12-07 01:51:50 +0100

    [ mail ] adding junk folder + error when it did not work

--- Arch-i3blocks/mail.sh
+++ Arch-i3blocks/mail.sh
@@ -2,20 +2,23 @@
 # mail - outputs number of new mails
 
 maildir="${HOME}/.mailfolder"
-mboxes="${maildir}/*/Inbox/new ${maildir}/*/Spam/new"
+mboxes="${maildir}/*/Inbox/new ${maildir}/*/Spam/new ${maildir}/*/Junk/new"
 mails=$(ls -1 ${mboxes} | grep -v new | sed '/^$/d' | wc -l)
 color=
 envel=
 
-if [[ ${mails} -gt 20 ]] ; then
+if [[ ${mails} -gt 30 ]] ; then
     color=\#fb4934
     envel=
 elif [[ ${mails} -gt 0 ]] ; then
     color=\#fabd2f
     envel=
-else
+elif [[ ${mails} -eq 0 ]] ; then
     color=\#8ec07c
     envel=
+else
+    color=\#fb4934
+    envel=ERROR
 fi
 
 diskColor="<span color=\"${color}\"><b>"
