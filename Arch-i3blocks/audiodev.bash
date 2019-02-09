aa1c889ff906be8d1b85706be4018e8604f1357e
Joschka Koester <jkoester@tzi.de>
2019-02-09 02:02:57 +0100

    [ audiodev ] testing a script to switch audiodevices via a single mouseclick

---
+++ Arch-i3blocks/audiodev.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+# audiodev - change audio device by pactl
+
+# button handling
+if [[ $1 == "1" ]] ; then
+    # default device (internal)
+    pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo+input:analog-stereo
+    pactl set-card-profile alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00 off
+fi
+
+if [[ $1 == "2" ]] ; then
+    # VoIP device (Headset, Logitech G933)
+    pactl set-card-profile alsa_card.pci-0000_00_1b.0 off
+    pactl set-card-profile alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00 output:analog-stereo+input:analog-mono
+fi
+
+tcolor=\#8ec07c
+devPath=${HOME}/.local/share/pulseaudiodevicechooser            # path to config folder
+devname=`cat ${devPath}/outdev`
+dname="Errör"
+
+if [[ ${devname} == *"pci-0000_00_1b.0"* ]] ; then
+    dname="Internal"
+elif [[ ${devname} == "alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00" ]] ; then
+    dname="Logitech G933"
+else
+    dname="Unknown"
+fi
+
+echo "<span color=\"${tcolor}\">${dname}</span>"
