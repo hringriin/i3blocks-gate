8c861d20fd25385fe104f1f42198eca3bfb13112
Joschka Koester <jkoester@tzi.de>
2019-02-09 02:44:47 +0100

    [ audiodev ] does not work as expected. needs rethinking

--- Arch-i3blocks/audiodev.bash
+++ Arch-i3blocks/audiodev.bash
@@ -4,14 +4,26 @@
 # button handling
 if [[ $1 == "1" ]] ; then
     # default device (internal)
-    pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo+input:analog-stereo
     pactl set-card-profile alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00 off
+    pactl set-card-profile alsa_card.pci-0000_01_00.1 off
+    pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo+input:analog-stereo
+    urxvt -name "PULSEAUDIO" -e ~/Repositories/github.com/hringriin/dotfiles/repo/pulseaudio/controlVolume.zsh setOutDev
 fi
 
 if [[ $1 == "2" ]] ; then
     # VoIP device (Headset, Logitech G933)
     pactl set-card-profile alsa_card.pci-0000_00_1b.0 off
+    pactl set-card-profile alsa_card.pci-0000_01_00.1 off
     pactl set-card-profile alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00 output:analog-stereo+input:analog-mono
+    urxvt -name "PULSEAUDIO" -e ~/Repositories/github.com/hringriin/dotfiles/repo/pulseaudio/controlVolume.zsh setOutDev
+fi
+
+if [[ $1 == "3" ]] ; then
+    # VoIP device (Headset, Logitech G933)
+    pactl set-card-profile alsa_card.pci-0000_00_1b.0 off
+    pactl set-card-profile alsa_card.usb-Logitech_Logitech_G933_Gaming_Wireless_Headset-00 off
+    pactl set-card-profile alsa_card.pci-0000_01_00.1 output:hdmi-stereo-extra1
+    urxvt -name "PULSEAUDIO" -e ~/Repositories/github.com/hringriin/dotfiles/repo/pulseaudio/controlVolume.zsh setOutDev
 fi
 
 tcolor=\#8ec07c
@@ -19,10 +31,13 @@ devPath=${HOME}/.local/share/pulseaudiodevicechooser            # path to config
 devname=`cat ${devPath}/outdev`
 dname="Errör"
 
+
 if [[ ${devname} == *"pci-0000_00_1b.0"* ]] ; then
     dname="Internal"
 elif [[ ${devname} == *"Logitech_Logitech_G933_Gaming_Wireless_Headset"* ]] ; then
     dname="Logitech G933"
+elif [[ ${devname} == *"pci-0000_01_00.1"* ]] ; then
+    dname="Samsung TV"
 else
     dname="Unknown"
 fi
